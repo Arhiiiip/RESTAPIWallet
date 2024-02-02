5e5d87b8e292d54bd10955e0909e90a041150547
Arhiiiip <rabokony@icloud.com>
2024-02-02 23:45:24 +0300

    Create sender coins and add parser for json body message

--- controllers/walletController.go
+++ controllers/walletController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	model "RESTAPIWallet/models"
+	parser "RESTAPIWallet/utils"
 	"encoding/json"
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
@@ -50,7 +51,84 @@ func (walletHandler *WalletHandler) CreateWallet(w http.ResponseWriter, r *http.
 }
 
 func (walletHandler *WalletHandler) SendMoney(w http.ResponseWriter, r *http.Request) {
-	//todo Реализовать логику перевода средств
+	var fromWalletID = mux.Vars(r)["walletId"]
+
+	var requestData struct {
+		To string `json:"to"`
+		/*Но честно говоря, лучшей практикой считаю любые валюты хранить в целых числах
+		Так операции будут наиболее точными, ведь проблема нецелых чисел в настоящее время всё ещё существует*/
+		Amount float64 `json:"amount"`
+	}
+
+	if err := parser.DecodeJSONBody(w, r, &requestData); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	tx := walletHandler.DB.Begin()
+
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+		}
+	}()
+
+	var fromWallet model.Wallet
+	if err := tx.First(&fromWallet, "id = ?", fromWalletID).Error; err != nil {
+		tx.Rollback()
+		http.Error(w, "Sender wallet not found", http.StatusNotFound)
+		return
+	}
+
+	if fromWallet.Balance < requestData.Amount {
+		tx.Rollback()
+		http.Error(w, "Insufficient funds", http.StatusBadRequest)
+		return
+	}
+
+	transaction := model.Transaction{
+		Time:   time.Now(),
+		From:   fromWalletID,
+		To:     requestData.To,
+		Amount: requestData.Amount,
+	}
+
+	if err := tx.Model(&fromWallet).Update("balance", fromWallet.Balance-requestData.Amount).Error; err != nil {
+		tx.Rollback()
+		http.Error(w, "Failed to update sender wallet balance", http.StatusInternalServerError)
+		return
+	}
+
+	var toWallet model.Wallet
+	if err := tx.First(&toWallet, "id = ?", requestData.To).Error; err != nil {
+		tx.Rollback()
+		http.Error(w, "Recipient wallet not found", http.StatusBadRequest)
+		return
+	}
+
+	if err := tx.Model(&toWallet).Update("balance", toWallet.Balance+requestData.Amount).Error; err != nil {
+		tx.Rollback()
+		http.Error(w, "Failed to update recipient wallet balance", http.StatusInternalServerError)
+		return
+	}
+
+	if err := tx.Create(&transaction).Error; err != nil {
+		tx.Rollback()
+		http.Error(w, "Failed to create transaction record", http.StatusInternalServerError)
+		return
+	}
+
+	tx.Commit()
+
+	response := struct {
+		Message string `json:"message"`
+	}{
+		Message: "Transaction successful",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
 
 func (walletHandler *WalletHandler) GetWalletHistory(w http.ResponseWriter, r *http.Request) {
